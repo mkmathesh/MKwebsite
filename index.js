b3996b4048c433b9f2762f57d5a6cf6ca958bc40
MATHESH M <mkmathesh302@gmail.com>
2026-01-31 23:34:41 +0530

    Add files via upload
    31/1/26

--- index.js
+++ index.js
@@ -4,4 +4,38 @@ function navlink(a)
     {
         open("#","_blank")
     }
+    else if(a=='contact')
+    {
+        open("#footer","_parent");
+    }
+} 
+let welimg=["img/code.jpg","img/fire.jpg","img/forest.jpg","img/galaxy.jpg","img/nebula.jpg"];
+let i=0;
+function welcomeimg()
+{
+    let wi=document.getElementById("welcomeimage");
+    wi.setAttribute("style","background-image: url("+welimg[i]+");")
+    i=(i+1)%welimg.length;
 }
+setInterval(welcomeimg,10000);
+
+let text="Welcome home!";
+let j=0
+let text2="May every corner of this space bring you knowledge";
+let k=0;
+let flag=false;
+function typewriter()
+{
+    if(j<text.length)
+    {
+        document.getElementById("welhome").innerHTML+=text.charAt(j);
+        j++;
+        setTimeout(typewriter,100);
+    }
+    else{
+        document.getElementById("welmeg").innerHTML+=text2.charAt(k);
+        k++;
+        setTimeout(typewriter,50);
+    }
+}
+onload=typewriter;
